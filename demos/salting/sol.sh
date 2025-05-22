77c0c74e9a6a9f6b72b2e6108b4a0d0bd5f5b14a
Callum Barker <cbarkr@gmail.com>
2025-05-21 17:32:35 -0700

    suppress  newline in salting demo

--- demos/salting/sol.sh
+++ demos/salting/sol.sh
@@ -1,3 +1,3 @@
-echo 'test' | sha256sum
-echo 'test<SALT-A>' | sha256sum
-echo 'test<SALT-B>' | sha256sum
+echo -n 'test' | sha256sum
+echo -n 'test<SALT-A>' | sha256sum
+echo -n 'test<SALT-B>' | sha256sum
